514eebb705ea7815ac7596c859f29fdec2562bf3
Narasimha-Rao-Meda <nrao1476@gmail.com>
2020-11-15 21:55:57 +0530

    Initial commit

--- managing_instances/describe_instances.py
+++
@@ -1,7 +0,0 @@
-import boto3
-
-ec2 = boto3.client('ec2')
-
-reponse = ec2.describe_instances()
-
-print(reponse)
--- readme.md
+++
@@ -1,1 +0,0 @@
-Access Amazon EC2 using the Amazon Web Services (AWS) SDK for Python. 
